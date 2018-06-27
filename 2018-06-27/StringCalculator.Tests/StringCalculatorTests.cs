0c200c555d1474b84eed65889fb7b9bc4c17a47b
Thabani Tembe <thabanitembe@hotmail.com>
2018-06-27 08:30:45 +0200

    kata start afresh

---
+++ 2018-06-27/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase(0, "")]
+        [TestCase(0, " ")]
+        [TestCase(0, null)]
+        public void GivenNoNumber_ShouldReturn0(int expected , string numbers) {
+            //Arrange
+            var sut = new StringCalculator();
+        
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(5, "5")]
+        [TestCase(8, "8")]
+        public void GivenOneNumber_ShouldReturnTheNumberItself(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(4, "1,3")]
+        [TestCase(5, "2,3 ")]
+        [TestCase(9, "4,5")]
+        public void GivenTwoNumbers_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(9, "4\n5")]
+        [TestCase(10, "4\n5\n1")]
+        public void GivenNumbesrWithNewLines_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(3, ";\n1;2")]
+        public void GivenNumbesrWithDifferentDelimeters_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 2018-06-27/StringCalculator/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public object Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers)) {
+                return 0;
+            }
+            if (numbers.Length > 1) {
+                var sum = 0;
+                string[] splitNumbers = numbers.Split(new Char[] { ',', '\n', ';', '/' },  StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < splitNumbers.Length; i++) {
+
+                    sum += Int32.Parse(splitNumbers[i]);
+                }
+                return sum;
+            }
+            return Int32.Parse(numbers);
+        }
+    }
+}
