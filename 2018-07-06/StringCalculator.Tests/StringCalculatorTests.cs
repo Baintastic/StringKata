d1441d1e11dcea2190c3403193518c1457126edc
Thabani Tembe <thabanitembe@hotmail.com>
2018-07-06 10:40:25 +0200

    finished kata took longer than 35 mins

---
+++ 2018-07-06/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,142 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase(0,"")]
+        [TestCase(0, " ")]
+        [TestCase(0, null)]
+        public void Add_GivenNoNumber_ShouldReturn0(int expected , string numbers){
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(10, "10")]
+        [TestCase(100, "100")]
+        public void Add_GivenOneNumber_ShouldReturnTheNumberItself(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(4, "1,3")]
+        [TestCase(40, "10,30")]
+        [TestCase(132, "100,32")]
+        public void Add_GivenTwoNumbers_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "1,2,3")]
+        [TestCase(20, "1,2,3,4,10")]
+        [TestCase(10, "1,,1,1,1,1,1,1,1,1,1")]
+        public void Add_GivenAnUnknownAmountOfNumbers_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "1\n2,3")]
+        [TestCase(6, "1\n2\n3")]
+        [TestCase(10, "1\n2,3\n4")]
+        public void Add_GivenNumbersWithNewLines_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(3, "//#\n1#2")]
+        [TestCase(3, "//@\n1@2")]
+        public void Add_GivenNumbersWithDifferentDelimiters_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("negatives not allowed : -1 ", "//@\n-1")]
+        [TestCase("negatives not allowed : -1 -2 ", "//@\n-1@-2")]
+        [TestCase("negatives not allowed : -1 -3 ", "//@\n-1@2@-3")]
+        public void Add_GivenNumbersWithNegatives_ShouldReturnNegativesNotAllowed(string expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = Assert.Throws<Exception>(()=> sut.Add(numbers));
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase(1, "//@\n1@1001")]
+        [TestCase(1015, "//@\n15@1000")]
+        [TestCase(1001, "//@\n2@999")]
+        public void Add_GivenNumbers_ShouldReturnTheSumOfThoseLessThan1001(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "//[***]\n1***2***3")]
+        [TestCase(6, "//[@@]\n1@@2@@3")]
+        [TestCase(6, "//[#####]\n1#####2#####3")]
+        public void Add_GivenNumbersWithDelimitersOfAnyLength_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "//[*][%]\n1*2%3")]
+        [TestCase(6, "//[**][%]\n1**2%3")]
+        [TestCase(8, "//[*][%][@@@]\n1*2%3@@@2")]
+        public void Add_GivenNumbersWithMultipleDelimitersOfAnyLength_ShouldReturnTheSum(int expected, string numbers)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+            //Act
+            var actual = sut.Add(numbers);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 2018-07-06/StringCalculator/StringCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+            if (numbers.Contains('-'))
+            {
+                var negatives = "";
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j].Equals('-'))
+                    {
+                        negatives += $"{numbers[j]}{ numbers[j + 1]} ";
+                    }
+                }
+                throw new Exception($"negatives not allowed : {negatives}");
+            }
+            if (numbers.Length == 1)
+            {
+                return Int32.Parse(numbers);
+            }
+
+            var sum = 0;
+            string[] splitNumbers = Split(numbers);
+
+            sum = Calculate(sum, splitNumbers);
+            return sum;
+        }
+
+        private static int Calculate(int sum, string[] splitNumbers)
+        {
+            for (int i = 0; i < splitNumbers.Length; i++)
+            {
+                var number = Int32.Parse(splitNumbers[i]);
+                if (number <= 1000)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+
+        private static string[] Split(string numbers)
+        {
+            numbers = numbers.Replace("[", "");
+            numbers = numbers.Replace("]", "");
+            return numbers.Split(new char[] { ',', '\n', ';', '/', '@', '%', '$', '#', '@', '!', '*' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
