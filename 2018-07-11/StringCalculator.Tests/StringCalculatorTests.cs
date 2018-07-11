1c59871787c53e19b7f0395b7414c1397dcba074
Thabani Tembe <thabanitembe@hotmail.com>
2018-07-11 08:21:34 +0200

    finished kata within 30 mins ,new implementation found

---
+++ 2018-07-11/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,150 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase(0,"")]
+        [TestCase(0, " ")]
+        [TestCase(0, null)]
+        public void Add_GivenEmptyString_ShouldReturn0(int expected , string input) {
+            //Arrange
+            var sut = new StringCalculator();
+            
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase(1 ,"1")]
+        [TestCase(21, "21")]
+        public void Add_GivenStringHavingOneNumber_ShouldReturnTheNumberItself(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5, "1,4")]
+        public void Add_GivenStringHavingTwoNumbers_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5, "1,1,1,1,1")]
+        [TestCase(100, "10,20,30,20,20")]
+        public void Add_GivenStringHavingUnknownAmountOfNumbers_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "1\n2,3")]
+        public void Add_GivenStringHavingNewLineDelimiter_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(3, "//*\n1*2")]
+        public void Add_GivenStringHavingCustomDelimiter_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("negatives not allowed : -1 ", "-1")]
+        [TestCase("negatives not allowed : -1 -2 -3 -7 ", "-1,-2,-3,4,5,6,-7")]
+        public void Add_GivenStringHavingNegativeNumbers_ShouldReturnNegativesNotAllowed(string expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = Assert.Throws<Exception>(()=>sut.Add(input));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase(2, "1001,2")]
+        [TestCase(1002, "1000,2")]
+        public void Add_GivenStringHavingNumbersMoreThan1000_ShouldReturnTheSumOfThoseLessThan1001(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "//[***]\n1***2***3")]
+        public void Add_GivenStringHavingDelimitersOfAnyLength_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "//[*][%]\n1*2%3")]
+        [TestCase(10, "//[***][%][#@@@]\n1***2%3#@@@1#@@@3")]
+        public void Add_GivenStringHavingMultipleCharDelimitersOfAnyLength_ShouldReturnTheSum(int expected, string input)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 2018-07-11/StringCalculator/StringCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public object Add(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+            CheckForNegativeNumbers(input);
+            if (input.StartsWith("//"))
+            {
+                var indexOfNewLine = input.IndexOf('\n');
+                input = input.Substring(indexOfNewLine);
+            }
+            int sum = CalculateSum(input);
+            return sum;
+        }
+
+        private static int CalculateSum(string input)
+        {
+            return input.Split(new char[] { ',', '\n', ';', '*', '!', '@', '#', '%', '&', '(', ')' }, StringSplitOptions.RemoveEmptyEntries).Select(part => Convert.ToInt32(part)).Where(num => num <= 1000).Sum();
+        }
+
+        private static void CheckForNegativeNumbers(string input)
+        {
+            if (input.Contains('-'))
+            {
+                var negatives = "";
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i].Equals('-'))
+                    {
+                        negatives += $"{input[i]}{input[i + 1]} ";
+                    }
+                }
+                throw new Exception($"negatives not allowed : {negatives}");
+            }
+        }
+    }
+}
