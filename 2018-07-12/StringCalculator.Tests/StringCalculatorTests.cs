78fa86a9d2624aea57b66c6e523960a650b403b2
Thabani Tembe <thabanitembe@hotmail.com>
2018-07-12 08:40:28 +0200

    daily kat finished in 32 mins

---
+++ 2018-07-12/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,150 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("", 0)]
+        [TestCase(" ", 0)]
+        [TestCase(null, 0)]
+        public void Add_GivenEmptyString_ShouldReturn0(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("21", 21)]
+        public void Add_GivenStringWithOneNumber_ShouldReturnTheNumberItself(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2", 3)]
+        public void Add_GivenStringWithTwoNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2,3,4,5", 15)]
+        [TestCase("1,2,3,4,5,15,20,50", 100)]
+        public void Add_GivenStringWithAnUnknownAmountOfNumbers_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        public void Add_GivenStringWithNewLineDelimiter_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//@\n1@2", 3)]
+        public void Add_GivenStringWithDifferentDelimiters_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("-1,2", "negatives not allowed : -1")]
+        [TestCase("-1,-2,3,4,5,-6", "negatives not allowed : -1,-2,-6")]
+        public void Add_GivenStringWithNegativeNumbers_ShouldReturnNegativesNotAllowed(string input, string expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = Assert.Throws<Exception>(()=>sut.Add(input));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase("1001,3", 3)]
+        [TestCase("1000,2", 1002)]
+        public void Add_GivenStringWithNumbersMoreThan1000_ShouldReturnTheSumOfThoseLessThan1001(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//[***]\n1***2***3", 6)]
+        public void Add_GivenStringWithDelimitersOfAnyLength_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        public void Add_GivenStringWithMultipleCharDelimiters_ShouldReturnTheSum(string input, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
